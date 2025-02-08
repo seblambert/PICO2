f625c386089e58f21417f38e7fb0d9b4c020f4de
seblambert <75882305+seblambert@users.noreply.github.com>
2025-02-08 16:55:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://github.com/seblambert/PICO2/tree/main"
+firmware_url = "https://github.com/seblambert/PICO2/main"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test_ota.py")
 
